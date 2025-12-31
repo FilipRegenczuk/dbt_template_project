41eaef5ed2fd5ce27f5ab209f0387985117e9778
Filip Regenczuk <f.regenczuk@gmail.com>
2025-12-31 09:07:38 +0000

    autogenerate stg models

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,20 @@
+with 
 
-select
-    id as customer_id,
-    first_name,
-    last_name
-from {{ source('jaffle_shop', 'customers') }}
+source as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        first_name,
+        last_name
+
+    from source
+
+)
+
+select * from renamed
